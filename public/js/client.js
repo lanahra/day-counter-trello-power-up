138c74523f239d84c547824eeb839a8ad36d320e
Leonardo Nahra <leonardonahra@gmail.com>
2020-05-01 20:42:37 -0300

    Add day counter card badge

--- public/js/client.js
+++ public/js/client.js
@@ -1,5 +1,25 @@
 /* global TrelloPowerUp */
 
-var Promise = TrelloPowerUp.Promise;
+function daysBetween(startDate, endDate) {
+  return Math.round((endDate - startDate) / (1000 * 60 * 60 * 24));
+}
 
-TrelloPowerUp.initialize({});
+function createdDateOf(cardId) {
+  return new Date(1000 * parseInt(id.substring(0, 8), 16));
+}
+
+function now() {
+  return Date.now();
+}
+
+TrelloPowerUp.initialize({
+  'card-badges': (t, opts) => {
+    return t.card('id').then(cardId => {
+      return [
+        {
+          text: daysBetween(createdDateOf(cardId), now()).toString()
+        }
+      ];
+    });
+  }
+});
