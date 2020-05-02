6ed9fc727146b50df810d6a97d40f3f6e7c29540
Leonardo Nahra <leonardonahra@gmail.com>
2020-05-02 00:14:54 -0300

    Add badge color and icon

--- public/js/client.js
+++ public/js/client.js
@@ -12,14 +12,37 @@ function now() {
   return Date.now();
 }
 
+function badgeColorFor(daysOpen) {
+  if (daysOpen < 5) {
+    return null;
+  } else if (daysOpen >= 5 && daysOpen < 10) {
+    return 'yellow';
+  } else if (daysOpen >= 10 && daysOpen < 14) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+}
+
+function iconFor(daysOpen) {
+  return daysOpen >= 5
+    ? './images/calendar-white.svg'
+    : './images/calendar-gray.svg';
+}
+
 TrelloPowerUp.initialize({
   'card-badges': (t, opts) => {
     return t.card('id').then(card => {
-      return [
-        {
-          text: daysBetween(createdDateOf(card), now()).toString()
-        }
-      ];
+      const daysOpen = daysBetween(createdDateOf(card), now());
+      return daysOpen > 0
+        ? [
+            {
+              text: daysOpen.toString(),
+              color: badgeColorFor(daysOpen),
+              icon: iconFor(daysOpen)
+            }
+          ]
+        : [];
     });
   }
 });
